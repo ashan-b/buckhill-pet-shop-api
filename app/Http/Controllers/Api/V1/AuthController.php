fa27ef7b44930c09c95a17ea943c382368ce0346
Ashan Beruwalage <ashan-b@live.com>
2022-07-24 20:57:30 +0530

    Added test auth route

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -1,1 +1,14 @@
 <?php
+
+use App\Http\Controllers\AppBaseController;
+
+class AuthController extends AppBaseController
+{
+    public function __construct()
+    {
+    }
+
+    public function testRoute(){
+        return "Test route.";
+    }
+}
