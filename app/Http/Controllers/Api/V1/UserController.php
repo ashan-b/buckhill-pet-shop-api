6c31487b4a638f6a901d5eddc20260ed02e15405
Ashan Beruwalage <ashan-b@live.com>
2022-07-26 22:23:48 +0530

    Added user logout

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\AppBaseController;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\V1\UserController\UserLoginRequest;
+use App\Http\Requests\Api\V1\UserController\UserLogoutRequest;
 use App\Http\Traits\JwtTokenHelper;
 use App\Http\Traits\ResponseGenerator;
 use App\Models\User;
@@ -22,14 +23,14 @@ class UserController extends Controller
      *     name="User",
      *     description="User API endpoint"
      * )
-     * /
+     ** /
 
-    /*
+    /**
      * @OA\Post(
      *     path="/api/v1/user/login",
      *     summary="Login an User account",
      *     tags={"User"},
-    *     @OA\RequestBody(
+     *     @OA\RequestBody(
      *     required=true,
      *         @OA\MediaType(
      *             mediaType="application/x-www-form-urlencoded",
@@ -69,7 +70,7 @@ class UserController extends Controller
      *         description="Internal server error"
      *     )
      * )
-     */
+     **/
     public function login(UserLoginRequest $request)
     {
         /*
@@ -94,6 +95,48 @@ class UserController extends Controller
             return $this->sendSuccess(["token" => $token]);
         } else {
             return $this->sendError("Failed to authenticate user", [], null, 422);
+        }
+    }
+
+
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/user/logout",
+     *     summary="Logout an User account",
+     *     tags={"User"},
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Response(
+     *         response=200,
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error"
+     *     )
+     * )
+     **/
+    public function logout(UserLogoutRequest $request)
+    {
+        $bearerToken = $request->bearerToken();
+        $tokenInvalidated = $this->invalidateJwtToken($bearerToken);
+
+        if ($tokenInvalidated == true) {
+            return $this->sendSuccess([]);
+        } else {
+            return $this->sendError("Invalid token.", [], null, 422);
         }
     }
 
