9ce00b5596994bc3e1dc0f3f063ec9f3f92106f4
Ashan Beruwalage <ashan-b@live.com>
2022-07-27 19:00:52 +0530

    Created orders migration

---
+++ database/migrations/2022_07_27_131912_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->char('uuid', 36)->unique();
+            $table->json('products');
+            $table->json('address');
+            $table->double('delivery_fee', 8, 2)->nullable();
+            $table->double('amount', 12, 2);
+            $table->timestamp('shipped_at')->nullable();
+            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('order_status_id')->constrained()->nullable();
+            $table->foreignId('payment_id')->constrained()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
