3301d93760f9d61e4d34b371d8dd0fa859f77bd1
Ashan Beruwalage <ashan-b@live.com>
2022-07-28 13:23:49 +0530

    Added api url to config

--- packages/ashan/currency-exchange-rate/config/currency_exchange_rate.php
+++ packages/ashan/currency-exchange-rate/config/currency_exchange_rate.php
@@ -2,5 +2,6 @@
 
 return [
     'base_currency' => env('CURRENCY_EXCHANGE_BASE_CURRENCY', 'EUR'),
-    'reference_rate_xml_url' => env('CURRENCY_EXCHANGE_REFERENCE_RATE_XML_URL', 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml')
+    'reference_rate_xml_url' => env('CURRENCY_EXCHANGE_REFERENCE_RATE_XML_URL', 'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'),
+    'api_url' => env('CURRENCY_EXCHANGE_URL', '/currency-exchange')
 ];
