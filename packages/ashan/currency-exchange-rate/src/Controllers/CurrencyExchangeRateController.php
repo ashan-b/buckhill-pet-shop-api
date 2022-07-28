06dce754d2fa7850e484f74af80ec15242d9c601
Ashan Beruwalage <ashan-b@live.com>
2022-07-28 13:22:46 +0530

    Created controller

---
+++ packages/ashan/currency-exchange-rate/src/Controllers/CurrencyExchangeRateController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Ashan\CurrencyExchangeRate\Controllers;
+
+use App\Http\Controllers\Controller;
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class CurrencyExchangeRateController extends Controller
+{
+    public static function convert(Request $request)
+    {
+        $amount = $request->amount;
+        $foreignCurrency = $request->currency;
+
+        try {
+            if ($amount <= 0.0) {
+                return response()->json(
+                    [
+                        'success' => 0,
+                        'data' => [],
+                        "error" => "Amount must be greater than 0.",
+                        "errors" => [],
+                        "trace" => []
+                    ],
+                    422
+                );
+            }
+
+            $baseCurrency = config('currency_exchange_rate.base_currency', 'EUR');
+            $referenceRateXmlUrl = config(
+                'currency_exchange_rate.reference_rate_xml_url',
+                'https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml'
+            );
+            $originalAmount = $amount;
+
+            $url = file_get_contents($referenceRateXmlUrl);
+            $xml = new \SimpleXMLElement($url);
+
+            $foreignCurrencyNode = $xml->xpath('//*[@currency="' . $foreignCurrency . '"]');
+
+            if ($foreignCurrencyNode == null) {
+                return response()->json(
+                    [
+                        'success' => 0,
+                        'data' => [],
+                        "error" => "Invalid foreign currency code.",
+                        "errors" => [],
+                        "trace" => []
+                    ],
+                    422
+                );
+            }
+            $exchangeRateResponse = [];
+            $foreignCurrencyRate = $foreignCurrencyNode[0]['rate'];
+            $exchangeRateResponse[] = [
+                "1 EUR" => $foreignCurrencyRate . " " . $foreignCurrency
+            ];
+
+            if ($baseCurrency !== "EUR") {
+                $baseCurrencyNode = $xml->xpath('//*[@currency="' . $baseCurrency . '"]');
+                if ($baseCurrencyNode == null) {
+                    return response()->json(
+                        [
+                            'success' => 0,
+                            'data' => [],
+                            "error" => "Invalid base currency code.",
+                            "errors" => [],
+                            "trace" => []
+                        ],
+                        422
+                    );
+                }
+                //Convert amount to EUR
+                $amount = $amount / $baseCurrencyNode[0]['rate'];
+                $exchangeRateResponse[] = [
+                    "1 EUR" => $baseCurrencyNode[0]['rate'] . " " . $baseCurrency
+                ];
+            }
+
+            $convertedAmount = round($amount * $foreignCurrencyRate, 2);
+
+
+            return response()->json(
+                [
+                    'success' => 1,
+                    'data' => [
+                        'base_currency' => $baseCurrency,
+                        "foreign_currency" => $foreignCurrency,
+                        'exchange_rate' => $exchangeRateResponse,
+                        'original_amount' => $originalAmount,
+                        'converted_amount' => $convertedAmount
+                    ],
+                    "error" => null,
+                    "errors" => [],
+                    "extra" => []
+                ],
+                200
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'success' => 0,
+                    'data' => [],
+                    "error" => $e->getMessage(),
+                    "errors" => [],
+                    "trace" => []
+                ],
+                500
+            );
+        }
+    }
+}
