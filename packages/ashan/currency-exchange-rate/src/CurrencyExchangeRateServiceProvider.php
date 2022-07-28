a2c82445b6ef2b524afa8374739665b1ee55fdac
Ashan Beruwalage <ashan-b@live.com>
2022-07-28 13:23:21 +0530

    Load routes

--- packages/ashan/currency-exchange-rate/src/CurrencyExchangeRateServiceProvider.php
+++ packages/ashan/currency-exchange-rate/src/CurrencyExchangeRateServiceProvider.php
@@ -2,26 +2,19 @@
 
 namespace Ashan\CurrencyExchangeRate;
 
+use App\Http\Controllers\CurrencyExchangeRateController;
 use Illuminate\Support\ServiceProvider;
 
 class CurrencyExchangeRateServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->loadRoutesFrom(__DIR__ . '/routes.php');
+
         $this->publishes(
             [
                 __DIR__ . '/../config/currency_exchange_rate.php' => config_path('currency_exchange_rate.php')
             ]
-        );
-    }
-
-    public function register()
-    {
-        $this->app->singleton(
-            CurrencyExchangeRate::class,
-            function () {
-                return new CurrencyExchangeRate();
-            }
         );
     }
 }
