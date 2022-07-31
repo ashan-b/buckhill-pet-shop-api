5d069e79a7523d6ac5b9c48725ab38df90e391d3
Ashan Beruwalage <ashan-b@live.com>
2022-07-31 23:28:28 +0530

    Added graph setter

--- packages/ashan/state-machine/src/Traits/StateMachine.php
+++ packages/ashan/state-machine/src/Traits/StateMachine.php
@@ -4,11 +4,46 @@ namespace Ashan\StateMachine\Traits;
 
 trait StateMachine
 {
+    protected $config = [];
+    protected $graph = [];
+    protected $property_path;
     protected $primaryKeyName;
 
     public function getPrimaryKeyName()
     {
         return $this->primaryKeyName;
+    }
+
+    public function setGraph($filename)
+    {
+        $path = storage_path("/state_machine_graphs/${filename}.json");
+        $graph = json_decode(file_get_contents($path));
+
+        $this->graph = $graph;
+        $this->property_path = $graph->property_path;
+        $this->primaryKeyName = $graph->state_primary_key;
+
+        //Set default state
+        if ($this->exists) {//If model exists load the current state.
+
+            $state = array_filter(
+                $graph->states,
+                function ($e) {
+                    return (
+                        property_exists($e, $this->primaryKeyName)
+                        &&
+                        $e->{$this->primaryKeyName} ==
+                        $this->{$this->property_path}->getMetadata()->{$this->primaryKeyName});
+                }
+            );
+        } else {
+            $defaultStateObject = array_filter(
+                $graph->states,
+                function ($e) {
+                    return (property_exists($e, 'initial') && $e->initial == true);
+                }
+            );
+        }
     }
 
 }
