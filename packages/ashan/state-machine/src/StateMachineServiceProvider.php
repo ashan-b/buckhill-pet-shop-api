f639ca70e3de0531ca41a07883f20f1160b01fba
Ashan Beruwalage <ashan-b@live.com>
2022-07-31 23:17:56 +0530

    Added state machine graph publishing

--- packages/ashan/state-machine/src/StateMachineServiceProvider.php
+++ packages/ashan/state-machine/src/StateMachineServiceProvider.php
@@ -13,5 +13,11 @@ class StateMachineServiceProvider extends ServiceProvider
                 __DIR__ . '/../config/state_machine.php' => config_path('state_machine.php')
             ]
         );
+
+        $this->publishes(
+            [
+                __DIR__ . '/../state_machine_graphs' => storage_path('state_machine_graphs'),
+            ]
+        );
     }
 }
