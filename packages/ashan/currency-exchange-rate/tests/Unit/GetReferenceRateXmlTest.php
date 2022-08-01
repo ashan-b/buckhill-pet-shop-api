16c195cde03fec6dbaa38379fb0c60444fbce8d9
Ashan Beruwalage <ashan-b@live.com>
2022-08-02 00:52:34 +0530

    Added unit test

---
+++ packages/ashan/currency-exchange-rate/tests/Unit/GetReferenceRateXmlTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ashan\CurrencyExchangeRate\Tests;
+use Ashan\CurrencyExchangeRate\Controllers\CurrencyExchangeRateController;
+use Tests\TestCase;
+
+class GetReferenceRateXmlTest extends TestCase
+{
+    /**
+     * A basic test example.
+     * @return void
+     */
+    public function test_get_reference_rate_xml()
+    {
+        $controller = new CurrencyExchangeRateController();
+        $xml = $controller->getReferenceRateXml();
+
+        $this->assertInstanceOf(\SimpleXMLElement::class,$xml);
+    }
+}
