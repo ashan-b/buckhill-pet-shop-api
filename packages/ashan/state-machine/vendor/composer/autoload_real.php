d3626dfa19951c6061f71290bd16b13e323f0927
Ashan Beruwalage <ashan-b@live.com>
2022-08-02 20:52:43 +0530

    Installed phpunit

--- packages/ashan/state-machine/vendor/composer/autoload_real.php
+++ packages/ashan/state-machine/vendor/composer/autoload_real.php
@@ -31,6 +31,25 @@ class ComposerAutoloaderInit92cc913b33e77f5210c53c81a9017833
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit92cc913b33e77f5210c53c81a9017833::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire92cc913b33e77f5210c53c81a9017833($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire92cc913b33e77f5210c53c81a9017833($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
