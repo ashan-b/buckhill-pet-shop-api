f99181eadfdbdab4dc30f314787be5e2a969c2f3
Ashan Beruwalage <ashan-b@live.com>
2022-08-05 03:02:03 +0530

    Use test model to test cases

--- src/packages/ashan/state-machine/tests/Feature/SetGraphToTestModelTest.php
+++ src/packages/ashan/state-machine/tests/Feature/SetGraphToTestModelTest.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace Ashan\StateMachine\Tests;
-use App\Models\Order;
+use Ashan\StateMachine\Models\TestModel;
 use Tests\TestCase;
 
-class SetGraphToOrderModelTest extends TestCase
+class SetGraphToTestModelTest extends TestCase
 {
     /**
      * Set graph to order model
      * @return void
      */
-    public function test_set_graph_to_order_model()
+    public function test_set_graph_to_test_model()
     {
-        $order = new Order;
+        $order = new TestModel();
         $order->setGraph("main_graph");
         self::assertEquals("main_graph", $order->getGraph()->graph);
     }
